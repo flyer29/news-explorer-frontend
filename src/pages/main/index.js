8635807468cd67a09a0161ef642719d9792faa68
Andrey Borodin <flyer29@bk.ru>
2020-10-09 23:20:57 +0300

    написал демо скрипт для попапов

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,6 +6,14 @@ const headerLinks = document.querySelectorAll('.header__link');
 const overlay = document.querySelector('.header__overlay');
 const headerNav = document.querySelector('.header__nav');
 const logoutButton = document.querySelector('.button_logout');
+const authorizationButton = document.querySelector('.button_authorization');
+const loginPopup = document.querySelector('.popup_type_login');
+const signUpButton = loginPopup.querySelector('.popup__link');
+const signUpPopup = document.querySelector('.popup_type_signup');
+const loginPopupCloseButton = loginPopup.querySelector('.popup__close');
+const signUpPopupCloseButton = signUpPopup.querySelector('.popup__close');
+const enterButton = signUpPopup.querySelector('.popup__link');
+
 
 headerButton.addEventListener('click', () => {
   headerNav.classList.toggle('show');
@@ -16,4 +24,32 @@ headerButton.addEventListener('click', () => {
   headerLinks.forEach((item) => {
     item.classList.toggle('header__link_menu');
   });
+});
+
+const showSignUpPopup = () => {
+  signUpPopup.classList.remove('hidden');
+  enterButton.addEventListener('click', () => {
+    signUpPopup.classList.add('hidden');
+    loginPopup.classList.remove('hidden');
+  });
+};
+
+const showLoginPopup = () => {
+  loginPopup.classList.remove('hidden');
+  signUpButton.addEventListener('click', () => {
+    loginPopup.classList.add('hidden');
+    showSignUpPopup();
+  });
+};
+
+authorizationButton.addEventListener('click', () => {
+  showLoginPopup();
+});
+
+loginPopupCloseButton.addEventListener('click', () => {
+  loginPopup.classList.add('hidden');
+});
+
+signUpPopupCloseButton.addEventListener('click', () => {
+  signUpPopup.classList.add('hidden');
 });
